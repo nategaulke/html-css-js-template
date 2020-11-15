e3aa93cfdaaad7edb125afd31d91e0c077e694b5
Nathanael Gaulke <nate.gaulke@knights.ucf.edu>
2020-11-15 14:00:24 -0500

    Add to script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, World from JavaScript!");
+let variable = 5 + 5.4 / 3 * 2 % 2;
+console.log(variable);
+variable = "hello\n world";
+console.log(variable);
+
+function sum(a, b) {
+    return a + b;
+};
+
+const number = 4;
+const string = "hello"
+
+const sum2 = (a, b) => a + b
+
+console.log(sum2(2, 3));
+// This will be 5
